116438caaf5cbf540809ac324b65fa38a8e8f85a
Chris Mayer <mayerxc@gmail.com>
2018-07-26 20:50:25 -0400

    Tweet component now installed and working

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
+    <h1>Error tweet:</h1>
+    <Tweet :id="'14'" error-message="This tweet could not be loaded" error-message-class="tweet--not-found"></Tweet>
+    <h1>With Computer:</h1>
+    <Tweet :id="'1022636072608694272'"></Tweet>
+    <h1>without computer</h1>
+    <Tweet :id="'1022636072608694272'" :options="{'cards': 'hidden'}"></Tweet>
+    
     <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <br />
     <AnotherComponent msg="what the hell is going on?"/>
+    <Tweet :id="'783943172057694208'"></Tweet>
   </div>
 </template>
 
 <script>
+import Tweet from 'vue-tweet-embed/tweet';
 import AnotherComponent from './components/AnotherComponent';
 import HelloWorld from './components/HelloWorld.vue';
 
@@ -15,7 +24,8 @@ export default {
   name: 'app',
   components: {
     HelloWorld,
-    AnotherComponent
+    AnotherComponent,
+    Tweet
   },
 };
 </script>
@@ -29,4 +39,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+Tweet {
+  /* display: block; */
+  float:right;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  text-align: center;
+}
+
 </style>
