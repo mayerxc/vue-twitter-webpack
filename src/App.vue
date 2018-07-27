51c1594be4589e5cc989d91d30766256937a5da6
Chris Mayer <mayerxc@gmail.com>
2018-07-26 20:22:31 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br />
+    <AnotherComponent msg="what the hell is going on?"/>
   </div>
 </template>
 
 <script>
+import AnotherComponent from './components/AnotherComponent';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
+    AnotherComponent
   },
 };
 </script>
---
+++ src/components/AnotherComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <h3>{{msg}}</h3>
+        <input type="text" v-model="title">
+        <h1>This button changes the title to something else:</h1>
+        <button v-on:click="addNewTitle">Submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AnotherComponent',
+    props: {
+    msg: String,
+    },
+    data: function(){
+        return {
+            title: 'this is a test!'
+        }
+    },
+    methods: {
+        addNewTitle: function(){
+            this.title = "changing from the button";
+        }
+    }
+
+};
+</script>
+
+<style scoped>
+h1 {
+    color:darkred;
+}
+
+</style>
