bdc1b7f05b379665a0def1127cfb68c38110cd7b
Chris Mayer <mayerxc@gmail.com>
2018-07-31 17:37:42 -0400

    Add feature to query user

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <input v-model="search" type="text">
-    <button v-on:click="getTweets(search)">Search!</button>
-    <div v-if="showTweets" style="display:flex; flex-direction: row; align-items: center;justify-content: center;flex-wrap:wrap;">
+    <h1>Search Tweets</h1>
+    <input placeholder="Search Term" v-model="search" type="text">
+    <button v-on:click="getTweets()">Search!</button>
+    
+    <div v-if="showTweets" style="display:flex; flex-direction: row; align-items:flex-start; justify-content:space-around;flex-wrap:wrap;">
       <div v-for="tweet in tweetArr" >
         <Tweet 
         :id="tweet.id_str"
@@ -10,6 +12,24 @@
         ></Tweet>
       </div>
     </div>
+    <div>
+      <h1>Search Users</h1>
+      <input placeholder="Search Twitter User" type="text" v-model="searchUser">
+      <button v-on:click="getUsers()">Search!</button>
+      <!-- <Timeline :id="'realDonaldTrump'" :sourceType="'profile'" :options="{ tweetLimit: '3', width: '255', align: 'center' }" error-message="This user could not be loaded" error-message-class="user--not-found" ></Timeline> -->
+      <div v-if="showUsers" style="display:flex; flex-direction: row; align-items:flex-start; justify-content:space-around;flex-wrap:wrap;">
+      <div v-for="user in userArr" >
+        <Timeline 
+          :id="user.screen_name" 
+          :sourceType="'profile'" 
+          :options="{ tweetLimit: '3', width: '255', align: 'center' }" 
+          error-message="This user could not be loaded" 
+          error-message-class="user--not-found" >
+        </Timeline>
+      </div>
+    </div>
+    </div>
+
 
 
     <h1>Error tweet:</h1>
@@ -32,6 +52,7 @@
 import axios from 'axios';
 // import cors from 'cors'
 import Tweet from 'vue-tweet-embed/tweet';
+import Timeline from 'vue-tweet-embed/timeline';
 import AnotherComponent from './components/AnotherComponent';
 import HelloWorld from './components/HelloWorld.vue';
 
@@ -40,18 +61,25 @@ export default {
   components: {
     HelloWorld,
     AnotherComponent,
-    Tweet
+    Tweet,
+    Timeline
   },
   data: function(){
     return {
-      search: '',
+      search: "",
+      searchUser: "",
       showTweets: false,
+      showUsers: false,
       tweetArr: [],
+      userArr: [],
     }
   },
+
   methods: {
     getTweets: function() {
-      self = this;
+      let self = this;
+      self.showTweets = false;
+      self.tweetArr = [];
       let url = `https://twitter-backend.herokuapp.com/query?search=${this.search}`;
       // let url = `http://localhost:3000/query?search=${this.search}`
       // let url = `http://api.icndb.com/jokes/random/10`
@@ -59,6 +87,19 @@ export default {
         console.log(response);
         self.tweetArr = response.data.statuses
         self.showTweets = true;
+      })
+    },
+
+    getUsers: function () {
+      let self = this;
+      self.userArr = [];
+      self.showUsers = false;
+      let url = `https://twitter-backend.herokuapp.com/user?user=${this.searchUser}`;
+      // let url = `http://localhost:3000/user?user=${this.searchUser}`;
+      axios.get(url).then( function(response) {
+        console.log(response)
+        self.userArr = response.data
+        self.showUsers = true;
       })
     }
   }
