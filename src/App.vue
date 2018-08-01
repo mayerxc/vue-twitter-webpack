485723e0ed4fbed3cfcfdad009d111a0c57a137f
Chris Mayer <mayerxc@gmail.com>
2018-07-31 22:11:19 -0400

    Add in Vue Material

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,96 @@
 <template>
   <div id="app">
-    <h1>Search Tweets</h1>
-    <input placeholder="Search Term" v-model="search" type="text">
-    <button v-on:click="getTweets()">Search!</button>
-    
-    <div v-if="showTweets" style="display:flex; flex-direction: row; align-items:flex-start; justify-content:space-around;flex-wrap:wrap;">
-      <div v-for="tweet in tweetArr" >
-        <Tweet 
-        :id="tweet.id_str"
-          :options="{width:'255', align:'center'}" 
-        ></Tweet>
-      </div>
-    </div>
     <div>
-      <h1>Search Users</h1>
-      <input placeholder="Search Twitter User" type="text" v-model="searchUser">
-      <button v-on:click="getUsers()">Search!</button>
-      <!-- <Timeline :id="'realDonaldTrump'" :sourceType="'profile'" :options="{ tweetLimit: '3', width: '255', align: 'center' }" error-message="This user could not be loaded" error-message-class="user--not-found" ></Timeline> -->
-      <div v-if="showUsers" style="display:flex; flex-direction: row; align-items:flex-start; justify-content:space-around;flex-wrap:wrap;">
-      <div v-for="user in userArr" >
-        <Timeline 
-          :id="user.screen_name" 
-          :sourceType="'profile'" 
-          :options="{ tweetLimit: '3', width: '255', align: 'center' }" 
-          error-message="This user could not be loaded" 
-          error-message-class="user--not-found" >
-        </Timeline>
-      </div>
+      
+      <md-tabs md-alignment="centered">
+        <md-tab md-label="Search Tweets">
+          <h1 class="md-title">Search Tweets</h1>
+          <div class="md-layout md-alignment-top-center">
+            <md-toolbar class="md-layout-item md-size-60 md-small-size-75 md-xsmall-size-100 md-layout md-alignment-top-center">
+      
+              <md-field>
+                <label>Search Term</label>
+                <md-input v-on:keyup.enter="getTweets()" v-model="search" type="text"></md-input>
+                <md-button v-on:click="getTweets()">
+                  <md-icon >search</md-icon>
+                </md-button>
+                
+              </md-field>
+            </md-toolbar>
+          </div>
+          <md-content class="md-layout md-alignment-top-center md-size-60">
+            <div class="md-layout md-alignment-top-center" v-if="showTweets">
+              <div class="md-layout-item" v-for="tweet in tweetArr" >
+                <Tweet 
+                  :id="tweet.id_str"
+                  :options="{width:'275', align:'center'}" 
+                >
+                  <md-progress-spinner :md-diameter="100" :md-stroke="10" md-mode="indeterminate"></md-progress-spinner>
+                </Tweet>
+              </div>
+            </div>
+          </md-content>
+        </md-tab>
+        <md-tab  md-label="Search for Twitter Users">
+          <div>
+            <h1 class="md-title">Search Users</h1>
+            <div class="md-layout md-alignment-top-center">
+              <md-toolbar class="md-layout-item md-size-60 md-small-size-75 md-xsmall-size-100 md-layout md-alignment-top-center">
+                <md-field>
+                  <label>Search Twitter Users</label>
+                  <md-input v-on:keyup.enter="getUsers()" v-model="searchUser" type="text"></md-input>
+                  <md-button v-on:click="getUsers()">
+                    <md-icon >search</md-icon>
+                  </md-button>
+                </md-field>
+              </md-toolbar>
+            </div>
+
+            <md-content class="md-layout md-alignment-top-center md-size-60">
+              <div class="md-layout md-alignment-top-center" v-if="showUsers">
+                <div class="md-layout-item" v-for="user in userArr" >
+                  <timeline 
+                    :id="user.screen_name" 
+                    :sourceType="'profile'" 
+                    :options="{ tweetLimit: '3', width: '255', align: 'center' }" 
+                    error-message="This user could not be loaded" 
+                    error-message-class="user--not-found" 
+                  >
+                    <md-progress-spinner :md-diameter="100" :md-stroke="10" md-mode="indeterminate"></md-progress-spinner>
+                  </timeline>
+                </div>
+              </div>
+            </md-content>
+          </div>
+        </md-tab>
+      </md-tabs>
+      
+        
     </div>
-    </div>
-
-
-
-    <h1>Error tweet:</h1>
-    <Tweet :id="'14'" error-message="This tweet could not be loaded" error-message-class="tweet--not-found"></Tweet>
-    <h1>With Computer:</h1>
-    <div style="width:400px;"><Tweet :id="'1022636072608694272'" :options="{width:'255', align:'center'}"></Tweet></div>
-    <!-- <Tweet :id="'1022636072608694272'" :options="{width:'255', align:'center'}"></Tweet> -->
-    <h1>without computer</h1>
-    <Tweet :id="'1022636072608694272'" :options="{'cards': 'hidden'}"></Tweet>
     
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <br />
-    <AnotherComponent msg="what the hell is going on?"/>
-    <Tweet :id="'783943172057694208'"></Tweet>
+    
+    
+    <!-- style="display:flex; flex-direction: row; align-items:flex-start; justify-content:space-around;flex-wrap:wrap;" -->
+    
+    
+
+
+
+    
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-// import cors from 'cors'
 import Tweet from 'vue-tweet-embed/tweet';
 import Timeline from 'vue-tweet-embed/timeline';
 import AnotherComponent from './components/AnotherComponent';
 import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
 
 export default {
   name: 'app',
@@ -82,7 +118,6 @@ export default {
       self.tweetArr = [];
       let url = `https://twitter-backend.herokuapp.com/query?search=${this.search}`;
       // let url = `http://localhost:3000/query?search=${this.search}`
-      // let url = `http://api.icndb.com/jokes/random/10`
       axios.get(url).then(function(response){
         console.log(response);
         self.tweetArr = response.data.statuses
@@ -123,5 +158,16 @@ Tweet {
   width: 50%;
   text-align: center;
 }
+
+
+
+/* .md-content {
+  width: 300px;
+  height: 200px;
+  display: inline-flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  } */
+
 
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    props: {
+        msg: String,
+    },
+    data: function(){
+        return {
+            title: 'this is a test!',
+            
+        }
+    },
+    methods: {
+        addNewTitle: function(){
+            this.title = "changing from the button";
+        }
+    }
+
+};
+</script>
+
+<style scoped>
+h1 {
+    color:darkred;
+}
+
+</style>
